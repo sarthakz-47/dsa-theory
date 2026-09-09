e0ec294466b2e798779c4e98366c71422130456d
sarthakz-47 <sarthakz.4100@gmail.com>
2026-09-09 13:21:00 +0530

    Inheritance

--- OOPS/oops.java
+++ OOPS/oops.java
@@ -31,6 +31,26 @@ class OOPS {
 
         Student4 s4 = new Student4("Aarti", 12);
         Student4 s5 = new Student4();
+
+        // Copy Constructor:
+        Car c1= new Car("XUV 700",2010);
+        Car c2 = new Car(c1);
+
+        System.out.println(c2.name);
+        System.out.println(c2.model);
+
+
+        // Ingeritance:
+        Animal a = new Animal();
+        a.eat();
+
+        Dog d = new Dog();
+        d.eat(); // not in Dog class but defined in Animal class
+        d.bark(); // defined in own Dog class
+
+        // multi level inheritance:
+        Cat c = new Cat();
+        c.eat();
     }
 }
 
@@ -123,5 +143,104 @@ class Student4 {
     // do not have any return type not even void
     // can only called once
     // memory allocation happens when new keyword is defined
-
 }
+
+// Encapsulation: copy constructors:
+class Car{
+    String name;
+    int model;
+
+    // Normal Constructor:
+    Car(String name,int model){
+        this.name = name;
+        this.model =model;
+    }
+
+    //  Copy Constructor:
+    Car(Car c){
+        this.name = c.name;
+        this.model = c.model;
+    }
+}
+
+// Encapsulation: Shallow copy aur deep copy khud se padho mujhe nahi samjha
+
+// Inheritance:
+// When a base class passes its properites and methods to derived class
+
+// Base / parent class:
+class Animal{
+    String name;
+    int age;
+    String color;
+
+    void eat(){
+        System.out.println("animal eats");
+    }
+}
+
+// Derived class / subclass:
+class Dog extends Animal{
+    int legs;
+
+    void bark(){
+        System.out.println("bhow!");
+    }
+}
+
+// 1.Single-level ineritance: when 1 based class passes its properties to one derived class
+// as above
+
+// 2.multi level inheritance:
+// when 1 base class passes to one derived class and then that derived class passes to another derived class
+
+// Base class:
+// class Animal{
+//     String name;
+//     int age;
+//     String color;
+
+//     void eat(){
+//         System.out.println("animal eats");
+//     }
+// }
+
+// Base class -> Derived Class
+class Mamal extends Animal{
+    int legs;
+
+    void wallk(){
+        System.out.println("mamal wallks");
+    }
+}
+
+// Base class-> Derived Class-> Derived Class
+class Cat extends Mamal{
+    String breed;
+
+    void speak(){
+        System.out.println("meow");
+    }
+}
+
+// 3.one base class and multiple derived classes:
+// parent class: Hall
+class Hall{
+    int number;
+    void fucntion(){
+        System.out.println("welcome");
+    }
+}
+
+// derived class 1: Hall-> Table
+class Table extends Hall{
+    int pos;
+    String color;
+}
+
+// derived class 2: Hall->Decoration
+class Decoration extends Hall{
+    float rating;
+}
+
+
